5538f1694b8328ba327cd13fe7cdb66c55df27dd
Alma <alma__tb@hotmail.com>
2023-02-17 18:04:01 -0800

    Add code to 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * Description: Print all numberd of base 10 twice
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	putchar('\n');
+	return (0);
+}
