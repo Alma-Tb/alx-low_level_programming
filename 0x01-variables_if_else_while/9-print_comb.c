59001056c7e4b7dca953da2f35ca8c68a1b6adc9
Alma <alma__tb@hotmail.com>
2023-02-17 18:38:42 -0800

    Add bettyvmodification 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main (void)
 {
 	int num;
 
-	for (num = 0; nun <= 9; num ++)
+	for (num = 0; num <= 9; num ++)
 	{
 		putchar((num % 10) +'0');
 		if (num == '9')
 			break;
-		putchar(',';
+		putchar(',');
 		putchar(' ');
 	}
 
 	putchar('\n');
-	Return (0)
+	return (0);
