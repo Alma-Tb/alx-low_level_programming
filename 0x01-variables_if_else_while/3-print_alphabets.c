dc6b1fe11671c49586a4230b5f7b079a2bdf4857
Alma <alma__tb@hotmail.com>
2023-02-17 17:34:47 -0800

    Add code to 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print  lowercase followed by  uppercase alphabet using putchar.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char l = 'a';
+	char L = 'A';
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+
+	while (L<= 'Z')
+	{
+		putchar(L);
+		L++;
+	}
+	putchar('\n');
+	return (0);
+}
