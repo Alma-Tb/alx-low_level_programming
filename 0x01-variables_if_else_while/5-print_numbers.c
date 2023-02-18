c961368fe9c4f0130393290a935f755ddd3a6d2c
Alma <alma__tb@hotmail.com>
2023-02-17 17:50:25 -0800

    Add code to 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main
+ * Description: Print numbers 0-10
+ * Return: 0
+ */
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
