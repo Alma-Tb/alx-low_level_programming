730347de8b7b8bff08887ced9f3c28e7a8a031c7
Alma <alma__tb@hotmail.com>
2023-02-20 14:08:12 -0800

    Add code to 4-puts.c & 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,17 @@
-#! /bin/bash
+#include<stdio.h>
+
+/**
+ *
+ * main - main block 
+ *
+ * Description: peint using printf
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return(0);
+}
