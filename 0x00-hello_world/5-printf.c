7695caea4019d9359b923ee0a2c84ed0a9cf8743
Alma <alma__tb@hotmail.com>
2023-02-20 14:38:29 -0800

    Apply betty to 4-puts.c 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
 /**
- *
  * main - main block
  *
  * Description: peint using printf
@@ -13,5 +12,5 @@ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return(0);
+	return (0);
 }
