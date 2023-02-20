2bf977da713a2378f1cc7da2170adc568676aa3f
Alma <alma__tb@hotmail.com>
2023-02-20 12:46:13 -0800

    Update 9-print_comb.c to produce required output

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,16 @@ int main (void)
 
 	for (num = 0; num <= 9; num ++)
 	{
-		putchar((num % 10) +'0');
-		if (num == '9')
-			break;
-		putchar(',');
-		putchar(' ');
+		if (num < 9 )
+		{
+			putchar((num % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+		else 
+			putchar((num % 10) + '0');
 	}
 
 	putchar('\n');
 	return (0);
+}
